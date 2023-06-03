f5524bd40fe91d482c2aef1ae9b627fb3b19b4a0
ZigZogger <ziggycooperhenry@gmail.com>
2023-06-03 13:01:38 -0700

    added script
    script updates OS, installs mySQL, mongoDB, and apache

---
+++ script.sh
@@ -0,0 +1,17 @@
+# this is my script!
+
+echo the script is running
+
+sudo apt-get update
+echo OS has been updated
+
+sudo apt-get install mysql
+echo mySQL has been installed
+
+sudo apt-get install mongodb.org
+echo mongoDB has been installed
+
+sudo apt-get install apache2
+echo apache has been installed
+
+echo the script is done!
