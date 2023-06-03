b998d1d86ad852fd1cecd2459de97561cb85dddb
ZigZogger <ziggycooperhenry@gmail.com>
2023-06-03 14:33:25 -0700

    some errors have been fixed but still doesn't install

--- script.sh
+++ script.sh
@@ -1,17 +1,7 @@
 # this is my script!
-
 echo the script is running
-
-sudo apt-get update
-echo OS has been updated
-
-sudo apt-get install mysql
-echo mySQL has been installed
-
-sudo apt-get install mongodb.org
-echo mongoDB has been installed
-
+sudo apt-get update --allow-insecure-repositories
+sudo apt-get install mysql-server
+sudo apt-get install mongodb-org
 sudo apt-get install apache2
-echo apache has been installed
-
 echo the script is done!
